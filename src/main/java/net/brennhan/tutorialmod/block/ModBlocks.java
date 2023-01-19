1031605c61ad0dbd98b6b7e408c123ab7607f452
Mideilty <789xxbrennm@gmail.com>
2023-01-19 11:18:47 -0600

    it now said import class today very odd

--- src/main/java/net/brennhan/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/brennhan/tutorialmod/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.brennhan.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.*;
 import net.minecraft.block.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
